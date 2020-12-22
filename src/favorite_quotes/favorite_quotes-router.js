9d5b2f5b52de287f5233e9fb74a4b99a58538e28
Justin Hager <coachjustinhager@gmail.com>
2020-12-22 12:19:35 -0800

    makes router specific to this project

--- src/favorite_quotes/favorite_quotes-router.js
+++ src/favorite_quotes/favorite_quotes-router.js
@@ -1,99 +1,118 @@
-// const express = require("express");
-// const ArticlesService = require("./articles-service");
-// const xss = require('xss');
+const express = require('express');
+const FavoriteQuotesServices = require('./favorite_quotes-services');
+const path = require('path');
+const xss = require('xss');
 
-// const articlesRouter = express.Router();
+const quotesRouter = express.Router();
 
-// articlesRouter
-//   .route("/")
-//   .get((req, res, next) => {
-//     ArticlesService.getAllArticles(req.app.get('db'))
-//       .then((articles) => {
-//         if (articles.length !== 0) {
-//           articles = articles.map(article => {
-//             return {
-//               id: article.id,
-//               style: article.style,
-//               title: xss(article.title), // sanitize title
-//               content: xss(article.content), // sanitize content
-//               date_published: article.date_published,
-//             };  
-//           });
-//         }
-//         return articles;
-//       })
-//       .then(articles => res.json(articles))
-//       .catch(next);
-//   })
-//   .post((req, res, next) => {
-//     const { title, content, style } = req.body;
-//     let newArticle = { 
-//       title, content, style  
-//     };
+const sanitizeQuote = quote => ({
+    id: quote.id,
+    content: xss(quote.content),
+    attribution: xss(quote.attribution),
+    source: xss(quote.source),
+    tags: xss(tags)
+})
 
-//     for (const [key, value] of Object.entries(newArticle)) {
-//       if(value == null) {
-//         return res.status(400).json({
-//           error: { message: `Missing ${key} in request body` }
-//         });
-//       }
-//     }
+quotesRouter
+  .route('/')
+  .get((req, res, next) => {
+    FavoriteQuotesServices.getAllQuotes(req.app.get('db'))
+      .then(quotes => res.json(quotes.map(sanitizeQuote)))
+      .catch(next);
+  })
+  .post((req, res, next) => {
+    const { content, attribution, source, tags } = req.body;
+    
+    let reqFields = { content }
+    let optionalFileds = { attribution, source, tags }
+    
+    // validate
+    for (const [key, value] of Object.entries(reqFields)) {
+      if(value === null) {
+        return res.status(400).json({
+          error: { message: `Missing ${key} in request body` }
+        });
+      }
+    }
 
-//     newArticle = { 
-//       title: xss(title),
-//       content: xss(content),
-//       style  
-//     };
+    let newQuote = {
+        ...reqFields,
+        ...optionalFileds
+    }
 
-//     ArticlesService.insertArticle(
-//       req.app.get('db'),
-//       newArticle
-//     )
-//       .then(article => {
-//         res
-//           .status(201)
-//           .location(`/articles/${article.id}`)
-//           .json(article);
-//       })
-//       .catch(next);
-//   });
+    FavoriteQuotesServices.insertQuote(
+      req.app.get('db'),
+      newQuote
+    )
+      .then(quote => {
+        res
+          .status(201)
+          .location(path.posix.join (req.originalUrl, `/${quote.id}`))
+          .json(sanitizeQuote(quote));
+      })
+      .catch(next);
+  });
 
-// articlesRouter
-//   .route('/:article_id')
-//   .all((req, res, next) => {
-//     ArticlesService.getById(
-//       req.app.get('db'),
-//       req.params.article_id
-//     )
-//       .then(article => {
-//         if (!article) {
-//           return res.status(404).json({
-//             error: { message: `Article doesn't exist` }
-//           });
-//         };
-//         res.article = article;
-//         next();
-//       })
-//       .catch(next);
-//   })
-//   .get((req, res, next) => {
-//     res.json({
-//       id: res.article.id,
-//       style: res.article.style,
-//       title: xss(res.article.title), // sanitize title
-//       content: xss(res.article.content), // sanitize content
-//       date_published: res.article.date_published,
-//     });
-//   })
-//   .delete((req, res, next) => {
-//     ArticlesService.deleteArticle(
-//       req.app.get('db'),
-//       req.params.article_id
-//     )
-//       .then(() => {
-//         res.status(204).end();
-//       })
-//       .catch(next);
-//   });
+quotesRouter
+  .route('/:quote_id')
+  .all((req, res, next) => {
+    FavoriteQuotesServices.getById(
+      req.app.get('db'),
+      req.params.quote_id
+    )
+      .then(quote => {
+        if (!quote) {
+          return res.status(404).json({
+            error: { message: `Quote doesn't exist` }
+          });
+        };
+        res.quote = quote;
+        next();
+      })
+      .catch(next);
+  })
+  .get((req, res, next) => {
+    return res.json(sanitizeQuote(res.quote));
+  })
+  .patch(bodyParser, (req,res,next) => {
+    const { content, attribution, source, tags } = req.body;
+    
+    let reqFields = { content }
+    let optionalFileds = { attribution, source, tags }
+    
+    // validate
+    for (const [key, value] of Object.entries(reqFields)) {
+      if(value === null) {
+        return res.status(400).json({
+          error: { message: `Missing ${key} in request body` }
+        });
+      }
+    }
 
-// module.exports = articlesRouter;
+    let updatedQuote = {
+        ...reqFields,
+        ...optionalFileds
+    }
+
+    FavoriteQuotesServices.updateQuote(
+        req.app.get('db'),
+        req.params.quote_id,
+        updatedQuote
+    )
+    .then(numRowsAffected => {
+        return res.status(204).end()
+    })
+    .catch(next)        
+})
+  .delete((req, res, next) => {
+    FavoriteQuotesServices.deleteArticle(
+      req.app.get('db'),
+      req.params.quote_id
+    )
+      .then(() => {
+        return res.status(204).end();
+      })
+      .catch(next);
+  });
+
+module.exports = quotesRouter;
